ad07e040e82dc1173a360f12d93dad87d29d0ea2
JamiNorja <jami.norja@outlook.com>
2023-10-09 17:48:33 +0300

    Eka

--- App.js
+++ App.js
@@ -1,11 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, StyleSheet, Button, TextInput } from 'react-native';
+import { useState, useEffect } from 'react';
+import * as Speech from 'expo-speech';
+import { Picker } from '@react-native-picker/picker';
 
 export default function App() {
+  const [text, setText] = useState('');
+  const [voices, setVoices] = useState([]);
+  const [selectedVoice, setSelectedVoice] = useState(null);
+
+  useEffect(() => {
+    getVoices();
+  }, []);
+
+  // Puhe
+  const speak = () => {
+    Speech.speak(text, {
+      voice: selectedVoice,
+    });
+  };
+
+  // Eri puhujat
+  const getVoices = async () => {
+    const availableVoices = await Speech.getAvailableVoicesAsync();
+    setVoices(availableVoices);
+    console.log(availableVoices);
+    setSelectedVoice(availableVoices[0]?.identifier || null);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.textbox}
+        placeholder='Write here'
+        value={text}
+        onChangeText={text => setText(text)}
+      />
+      <Picker
+        selectedValue={selectedVoice}
+        onValueChange={(value) => setSelectedVoice(value)}>
+        {voices.map((voice, index) => (
+          <Picker.Item
+            key={index}
+            label={voice.name}
+            value={voice.identifier}
+          />
+        ))}
+      </Picker>
+      <View style={styles.button}>
+        <Button
+          color='white'
+          title='PRESS TO HEAR TEXT'
+          onPress={speak}
+          disabled={!text || !selectedVoice}
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +62,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  textbox: {
+    justifyContent: 'center',
+    borderWidth: 2,
+    marginLeft: 60,
+    marginRight: 60,
+    height: 60,
+  },
+  button: {
+    backgroundColor: 'blue',
+    marginTop: 10,
+    marginRight: 90,
+    marginLeft: 90,
   },
 });
